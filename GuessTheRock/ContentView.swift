c8f73e0d255b1a553213154f2a23a2f713c84838
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-08-29 14:36:47 +0300

    Basic UI

--- GuessTheRock/ContentView.swift
+++ GuessTheRock/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    let moves = ["Rock", "Paper", "Scissors"]
+    let winningMoves = ["Paper", "Scissors", "Rock"]
+    
+    @State private var score = 0
+    @State private var appChoice = Int.random(in: 0..<3)
+    @State private var shouldPlayerWin = Bool.random()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [.purple, .indigo, .blue], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            VStack {
+                Text("Score: \(score)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                Text("You are attacked with \(moves[appChoice]).")
+                    .foregroundStyle(.white)
+                    .font(.headline)
+                Text("You should \(shouldPlayerWin ? "win" : "lose").")
+                    .foregroundStyle(.white)
+                    .font(.subheadline)
+                Group {
+                    Button("🪨 Rock") {
+                        
+                    }
+                    .font(.largeTitle.bold())
+                    Button("📄 Paper") {
+                        
+                    }
+                    .font(.largeTitle.bold())
+                    Button("✂️ Scissors") {
+                        
+                    }
+                    .font(.largeTitle.bold())
+                }
+                .buttonStyle(.bordered)
+                .foregroundStyle(.white)
+            }
         }
-        .padding()
+    }
+    
+    func analyzePlayerMove(move: Int) {
+        if moves[move] == winningMoves[appChoice] {
+            if shouldPlayerWin {
+                score += 1
+            }
+        }
+        
+        if !shouldPlayerWin {
+            score += 1
+        }
     }
 }
 
