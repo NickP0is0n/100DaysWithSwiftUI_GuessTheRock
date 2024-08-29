3cf41a5dd433bfa4e1b9b9d70e20706f7d8f6107
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-08-29 13:59:32 +0300

    Initial Commit

---
+++ GuessTheRock/GuessTheRockApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheRockApp.swift
+//  GuessTheRock
+//
+//  Created by Mykola Chaikovskyi on 29.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheRockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
